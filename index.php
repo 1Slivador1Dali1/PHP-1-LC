c53f2ffa94d9a19c04fba5a772d5b1ab418a20d8
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-07-05 23:30:36 +0300

    Типы данных

--- index.php
+++ index.php
@@ -7,3 +7,21 @@ echo $num;
 $num += 10;
 echo $num;
 ?>
+<br>
+<?php 
+$str = "Hello World $num"; // С двойными ковычками происходит преобразование переменной в ее содержимое. А с одинарными нет
+echo $str;
+?>
+<br>
+<?php 
+$str2 = " 12p2p";
+echo $str . $str2; // Конкатенация строк не плюс . а точка!!!
+?>
+<br>
+<?php 
+$bool = true;
+$arr = [10, 'Hello World'];
+echo $arr[1];
+$null = null; //Пустота или нет значения
+
+?>
